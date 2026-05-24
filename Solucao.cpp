f305bf010ef1b3cc20974d7a0e44271c9abf3c05
LaraGGomes <laragomes471@gmail.com>
2026-05-24 19:13:05 -0300

    estrutura e funções da matriz de subseq

--- Solucao.cpp
+++ Solucao.cpp
@@ -11,5 +11,33 @@ void calcularCusto(Data& data, Solucao *s) {
     s->cost = 0;
     for (int i = 0; i < s->sequence.size() - 1; i++)
         s->cost += data.d(s->sequence[i], s->sequence[i+1]);
-        // o getDistance(x, y) é o matrizAdj[x][y]
 }
+
+void updateAllSubseq(Data& data, Solucao *s, vector<vector<Subsequence>>& subseq_matrix) {
+    int n = s->sequence.size(); // num de subsequências formadas por apenas 1 nó
+
+    // subsequências de nó único
+    for (int i = 0; i < n; i++) {
+        int v = s->sequence[i];
+        subseq_matrix[i][i].C = 0;
+        subseq_matrix[i][i].T = 0;
+        subseq_matrix[i][i].W = (i > 0);
+        subseq_matrix[i][i].first = s->sequence[i];
+        subseq_matrix[i][i].last = s->sequence[i];
+    }
+
+    // concatenações de subsequências
+    for (int i = 0; i < n; i++) {
+        for (int j = i+1; j < n; j++)
+            subseq_matrix[i][j] = Subsequence::Concatenate(data, subseq_matrix[i][j-1], subseq_matrix[j][j]);
+    }
+
+    // subsequências invertidas
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = i - 1; j >= 0; j--)
+            subseq_matrix[i][j] = Subsequence::Concatenate(data, subseq_matrix[i][j+1], subseq_matrix[j][j]);
+    }
+}
+
+// função pra atualizar apenas as subsequências que mudaram após um movimento
+void updateSubseq(Data& data, Solucao *s, vector<vector<Subsequence>>& subseq_matrix);
--- Solucao.h
+++ Solucao.h
@@ -12,8 +12,30 @@ typedef struct Solucao {
     double cost;
 } Solucao;
 
+typedef struct Subsequence {
+    // valores auxiliares
+    double T, C;    // duração, custo acumulado
+    int W;          // custo de atraso
+    int first, last;
+
+    // função Concatenate()
+    inline static Subsequence Concatenate(Data& data, Subsequence &sigma_1, Subsequence &sigma_2) {
+        Subsequence sigma;
+        double temp = data.getDistance(sigma_1.last,sigma_2.first);
+        sigma.W = sigma_1.W + sigma_2.W;
+        sigma.T = sigma_1.T + temp + sigma_2.T;
+        sigma.C = sigma_1.C + sigma_2.W * (sigma_1.T + temp) + sigma_2.C;
+        sigma.first = sigma_1.first;
+        sigma.last = sigma_2.last;
+
+        return sigma;
+    }
+} Subsequence;
+
 void exibirSolucao(Solucao *s);
 void calcularCusto(Data& data, Solucao *s);
+void updateAllSubseq(Data& data, Solucao *s, vector<vector<Subsequence>>& subseq_matrix);
+void updateSubseq(Data& data, Solucao *s, vector<vector<Subsequence>>& subseq_matrix);
 
 #define d(x, y) getDistance(x, y)
 // pra simplificar o getDistance, que calcula o custo entre dois vértices
